0005f9a5fcae8104a9980c14917e1a2e1e05e504
Kiran Sarode <ksarode.pro@gmail.com>
2025-12-09 11:25:02 +0530

    decorator pattern added

---
+++ decorator_pattern/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace decorator_pattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Margaritta normalPrizza = new Margaritta();
+            System.Console.WriteLine(normalPrizza.GetPizza());
+
+            ChickenPizza chickenPizza = new ChickenPizza(normalPrizza);
+            System.Console.WriteLine(chickenPizza.GetPizza());
+        }
+    }
+}
+
+public interface IPizza
+{
+    public string GetPizza();
+}
+
+public class Margaritta : IPizza
+{
+    public string GetPizza()
+    {
+        return "Perfect fluffy bready + mozerella Cheeze + Unique Tomato Sause";
+    }
+}
+
+public abstract class PizzaDecorator : IPizza
+{
+    private IPizza _pizza;
+
+    public PizzaDecorator(IPizza pizza)
+    {
+        this._pizza = pizza;
+    }
+
+    public virtual string GetPizza()
+    {
+        return _pizza.GetPizza();
+    }
+}
+
+public class ChickenPizza : PizzaDecorator
+{
+    private IPizza _pizza;
+
+    public ChickenPizza(IPizza pizza) : base(pizza)
+    {
+        this._pizza = pizza;
+    }
+
+    /*public override string GetPizza()
+    {
+        return base.GetPizza() + " + Tandoori Chicken"
+    }*/
+
+    public override string GetPizza() => base.GetPizza() + " + Tandoori Chicken";
+}
+
